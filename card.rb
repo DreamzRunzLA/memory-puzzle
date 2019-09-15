3faed3ef8d582a563e54cea58e105def96d479fb
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-09-14 19:33:18 -0600

    Finished card class

--- card.rb
+++ card.rb
@@ -1,3 +1,24 @@
 class Card
+    attr_accessor :face_value, :face_up
 
+    def initialize
+        @face_value = ''
+        @face_up = false
+    end
+
+    def hide
+        self.face_up = false
+    end
+
+    def reveal
+        self.face_up = true
+    end
+
+    def to_s
+        return self.face_value
+    end
+
+    def ==(card)
+        return self.to_s == card.to_s
+    end
 end
