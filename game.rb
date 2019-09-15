fb13684f7a3b7cd6d220a89f1ccea6657ef7bdb4
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-09-15 14:45:49 -0600

    got play function half working

--- game.rb
+++ game.rb
@@ -1,3 +1,32 @@
+require_relative "board"
+require_relative "card"
+
 class Game
+    def initialize
+        @board = Board.new
+        @board.populate
+        @previous_pos = []
+    end
+
+    def play
+        # until @board.won? == true
+            system("clear")
+            @board.render
+            puts "Enter a row and column!"
+            input = gets.chomp
+            x = input[0]
+            y = input[-1]
+            guessed_pos = []
+            guessed_pos << x.to_i
+            guessed_pos << y.to_i
+        # end
+        return guessed_pos
+    end
+
+    def make_guess(arrayGuess)
+
+
+    end
+
 
 end
